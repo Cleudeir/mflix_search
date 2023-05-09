38f9e72b94167323021a231e974e7e0273c25916
Cleudeir <cleudeirsilva@gmail.com>
2023-05-08 22:41:49 -0300

    up

--- src/app/[type]/page.tsx
+++ src/app/[type]/page.tsx
@@ -9,10 +9,7 @@ export default async function HomePage({
 }: Props): Promise<JSX.Element> {
   const type = params.type;
   const url = `${process.env.NEXT_PUBLIC_BACK_URL}/map/${type}`;
-  console.log(url);
   const resp = await fetch(url);
   const save = await resp.json();
-  console.log("save: ", save.length);
-
   return (<Home save={save} type={type} />);
 }
