c0c19e3ec1497535b5a196f021dfc8663f39e2d2
Cleudeir <cleudeirsilva@gmail.com>
2023-05-08 23:15:57 -0300

    up

--- src/utils/noCors.ts
+++ src/utils/noCors.ts
@@ -1,25 +1,46 @@
 "use client";
+
+async function fetchTimeout(
+  url: RequestInfo | URL,
+  options?: RequestInit | undefined,
+  timeout = 10000
+): Promise<any> {
+  return await Promise.race([
+    fetch(url, options),
+    new Promise((solve, reject) =>
+      setTimeout(() => reject(new Error("timeout")), timeout)
+    ),
+  ]);
+}
+
 async function noCors(
   input: string,
   init?: RequestInit | undefined
 ): Promise<any> {
-  let url = "/api/" + input.split(":").join("__").replace("//", "/");  
-  if (!init) {   
-    const resp = await fetch(url);
-    const data = await resp.json();  
-    if (data.results) {    
-      return data.results;
-    } 
-    return data;
-  } else {    
-    const resp = await fetch(url, init);
-    const data = await resp.json();    
-    if (data.results) {     
-      return data.results;
-    } 
-    return data;
+  let url = "/api/" + input.split(":").join("__").replace("//", "/");
+  if (!init) {
+    try {
+      const resp = await fetchTimeout(url);
+      const data = await resp.json();
+      if (data.results) {
+        return data.results;
+      }
+      return data;
+    } catch (error: any) {
+      return { error: error.message, url };
+    }
+  } else {
+    try {
+      const resp = await fetchTimeout(url, init);
+      const data = await resp.json();
+      if (data.results) {
+        return data.results;
+      }
+      return data;
+    } catch (error: any) {
+      return { error: error.message, url };
+    }
   }
-
 }
 
 export default noCors;
