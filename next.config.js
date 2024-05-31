35d1171f54bb6d0d465e66eb5c06cb9849c24810
Cleudeir <cleudeirsilva@gmail.com>
2024-05-30 23:54:52 -0300

    up

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     return [
       {
         source: "/",
-        destination: "/movie",
+        destination: "/movie/category/em-alta",
         permanent: true,
       },
     ];
